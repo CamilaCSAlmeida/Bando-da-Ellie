02178a61e8b20f2f3a70ac70aa6540807deb4d10
CamilaCSAlmeida <camilacsalmeida5@gmail.com>
2026-04-15 19:32:17 -0300

    teste sem carrossel

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,7 @@ export default function Home() {
   return (
     <main>
 
-      {/* CARROSSEL COM ALTURA FIXA */}
-      <div style={{
-        height: "260px",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
-      }}>
-        <Carrossel />
-      </div>
+      {/* <Carrossel /> */}
 
       <h1 style={{ color: "#d88c9a" }}>
         Bem-vindo ao nosso cantinho 💕
